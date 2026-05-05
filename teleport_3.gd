adb4365aa8dd9ca34e9ada70d4498ae476c78799
akshatsingh1427 <singhakshatamlesh@gmail.com>
2026-05-05 09:45:41 +0530

    Change teleport target from Player to Bot and adjust time scale

--- teleport_3.gd
+++ teleport_3.gd
@@ -1,23 +1,23 @@
 extends Area2D
 
-# Path of the next level
+
 @export var next_level: String = "res://scenes/game_3b.tscn"
 
 func _ready():
 	monitoring = true
-	monitorable = true
+	monitorable = false
 
 
 func _on_body_entered(body):
 	# Only Player can teleport
-	if body.name != "Player":
+	if body.name != "Bot":
 		return
 
 	print("Player entered teleport")
 	print("Loading:", next_level)
 
 	# Safety reset
-	Engine.time_scale = 1.0
+	Engine.time_scale = 3.0
 
 	# Change scene (teleport)
 	get_tree().change_scene_to_file(next_level)
